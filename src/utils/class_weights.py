ed9137edc6d9478a6a29933a49e9eca38070b392
PantellaA <pantella.2053151@studenti.uniroma1.it>
2025-12-07 13:17:16 +0100

    Create class_weights.py

---
+++ src/utils/class_weights.py
@@ -0,0 +1,20 @@
+import numpy as np
+import torch
+from sklearn.utils.class_weight import compute_class_weight
+
+
+def compute_class_weights(train_dataset, device):
+  
+    targets = train_dataset.targets
+    classes = np.unique(targets)
+
+    weights = compute_class_weight(
+        class_weight="balanced",
+        classes=classes,
+        y=targets
+    )
+
+    weights = torch.tensor(weights, dtype=torch.float32).to(device)
+
+    print(f"[INFO] Computed class weights: {weights.tolist()}")
+    return weights
