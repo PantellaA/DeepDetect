15bf5980708b6d0767c89a916cc99e7196525934
PantellaA <pantella.2053151@studenti.uniroma1.it>
2025-12-07 11:46:27 +0100

    Create seed.py

---
+++ src/utils/seed.py
@@ -0,0 +1,15 @@
+import numpy as np
+import torch
+import random
+import os
+
+def set_global_seed(seed=42):
+    print(f" Set global seed: {seed}")
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    os.environ['PYTHONHASHSEED'] = str(seed)
