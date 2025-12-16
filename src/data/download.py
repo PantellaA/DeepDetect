62e52b777406540ba5ee60678055e61961e53e00
PantellaA <pantella.2053151@studenti.uniroma1.it>
2025-12-16 15:51:35 +0100

    Add docstring for download_raw_dataset function
    Added docstring to download_raw_dataset function.

--- src/data/download.py
+++ src/data/download.py
@@ -3,6 +3,9 @@ import kagglehub
 
 
 def download_raw_dataset(dataset_name: str = "ayushmandatta1/deepdetect-2025") -> Path:
+    """
+    Downloads the raw dataset from Kaggle using kagglehub and returns the path to the source data directory.
+    """
     path = kagglehub.dataset_download(dataset_name)
     ddata = Path(path) / "ddata"
 
